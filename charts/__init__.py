1535f96b8b42446c4a1bbd2093d90c18fad1d5c7
Bicalho <74017831+LuizBicalho3508@users.noreply.github.com>
2026-08-28 12:07:38 -0400

    feat: adiciona pacote de graficos amCharts

---
+++ charts/__init__.py
@@ -0,0 +1,5 @@
+"""Componentes visuais do Meu Financeiro."""
+
+from .amcharts import render_balance_chart, render_expense_donut, render_flow_chart
+
+__all__ = ["render_flow_chart", "render_expense_donut", "render_balance_chart"]
